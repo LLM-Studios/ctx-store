f37b40fef442a5ba6ec6ea2e11a694ae17b27454
schobele <leoschoberwalter@gmail.com>
2024-12-06 13:51:43 +0100

    update client

--- client/client.ts
+++ client/client.ts
@@ -2,7 +2,6 @@
 
 import { MessageRole } from "@prisma/client";
 import * as fetchClient from "./generated/index";
-import { CreateThreadData, UpdateThreadData } from "./generated/index";
 
 const getThread = (threadId: string, includeMessages = true) => {
 	return fetchClient
@@ -17,11 +16,14 @@ const listThreads = () => {
 	return fetchClient.listThreads().then((res) => res.data);
 };
 
-const createThread = (data?: CreateThreadData["body"]) => {
+const createThread = (data?: { title?: string; metadata?: any }) => {
 	return fetchClient.createThread({ body: data ?? {} }).then((res) => res.data);
 };
 
-const updateThread = (threadId: string, data: UpdateThreadData["body"]) => {
+const updateThread = (
+	threadId: string,
+	data: { title?: string; metadata?: any }
+) => {
 	return fetchClient
 		.updateThread({
 			path: { threadId },
@@ -63,7 +65,7 @@ const listMessages = (
 		.then((res) => res.data);
 };
 
-const createMessages = (
+const addMessages = (
 	threadId: string,
 	data: {
 		role: MessageRole;
@@ -74,6 +76,24 @@ const createMessages = (
 	return fetchClient
 		.createMessages({
 			body: data,
+			path: { threadId },
+		})
+		.then((res) => res.data);
+};
+
+const addUserMessage = (threadId: string, content: string) => {
+	return fetchClient
+		.createMessages({
+			body: [{ role: "user", content }],
+			path: { threadId },
+		})
+		.then((res) => res.data);
+};
+
+const addAssistantMessage = (threadId: string, content: string) => {
+	return fetchClient
+		.createMessages({
+			body: [{ role: "assistant", content }],
 			path: { threadId },
 		})
 		.then((res) => res.data);
@@ -116,7 +136,9 @@ export const createClient = (config?: { baseUrl?: string }) => {
 		deleteThread,
 		getMessage,
 		listMessages,
-		createMessages,
+		addMessages,
+		addUserMessage,
+		addAssistantMessage,
 		updateMessage,
 		deleteMessage,
 	};
